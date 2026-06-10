3c1a44586f8eb7d92317f2a57d22a695c1bfc3ed
ICECREAMMAN87 <kazakevichserg87@gmail.com>
2026-06-10 11:48:16 +0200

    Add file .py

---
+++ clone_tool.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.title("🧩 Snowflake DB Clone Tool")
+target_db = st.text_input("Target DB name", "Streamlit_Sergey")
+source_db = st.text_input("Clone from DB", "DEV")
+owner_role = st.selectbox("Owner role", ["DEVELOPMENT", "SYSADMIN", "ACCOUNTADMIN"])
+readonly_role = st.text_input("Read-only role", "read_only_role")
+st.warning(f"⚠️ {target_db} already exists (owner: {owner_role}). It will be dropped and recreated if you proceed.")
+if st.button("Refresh SQL"):
+    sql_script = f"""
+DROP DATABASE IF EXISTS {target_db};
+CREATE OR REPLACE DATABASE {target_db} CLONE {source_db};
+USE ROLE {owner_role};
+GRANT OWNERSHIP ON DATABASE {target_db} TO ROLE {owner_role} REVOKE CURRENT GRANTS;
+GRANT USAGE ON DATABASE {target_db} TO ROLE {readonly_role}
+GRANT USAGE ON ALL SCHEMAS IN {target_db} TO ROLE {readonly_role}
+GRANT SELECT ON ALL TABLES IN {target_db} TO ROLE {readonly_role}"""
+    st.subheader("Generated SQL")
+    st.markdown("### Please, always check db names you set up!")
+    st.code(sql_script, language="sql")
+    st.button("🚀 Execute SQL")
